0c7ae64d5e9ff0d913e43f00c57f25c11707745f
volodjangrodno <parthnersclub.ru@gmail.com>
2024-06-20 10:55:36 +0300

    LessonVD07dz correct

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 def index():
     weather = None
     news = None
-    quotes = None
+    quote = None
     if request.method == 'POST':
         city = request.form['city']
         weather = get_weather(city)
         news = get_news()
-        quotes = random_quote()
-    return render_template('index.html', weather=weather, news=news, quotes=quotes)
+        quote = random_quote()
+    return render_template('index.html', weather=weather, news=news, quote=quote)
 
 
 
@@ -24,16 +24,22 @@ def get_weather(city):
     response = requests.get(url)
     return response.json()
 
+@app.route('/news')
 def get_news():
     api_key = '0778a83d9485492197b3d62ce436c0fe'
     url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}'
     response = requests.get(url)
     return response.json().get('articles', [])
 
+@app.route('/quotes')
 def random_quote():
     url = 'https://api.quotable.io/random'
     response = requests.get(url)
-    return response.json().get('content', 'author')
+    if response.status_code == 200:
+        quote = response.json()
+        return render_template('quotes.html', content=quote['content'], author=quote['author'])
+    else:
+        return render_template('quotes.html', content='Error retrieving quote', author='')
 
 if __name__ == '__main__':
     app.run(debug=True)
